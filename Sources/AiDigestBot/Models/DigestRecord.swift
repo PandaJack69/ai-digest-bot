f93482c1f49d3f658242408ebdcc7d3c5cd59a59
PandaJack69 <kevinartan2005@gmail.com>
2026-09-04 09:26:34 +0700

    chore: added DigestRecord model and DigestArchiveStore service

---
+++ Sources/AiDigestBot/Models/DigestRecord.swift
@@ -0,0 +1,7 @@
+//
+//  DigestRecord.swift
+//  AiDigestBot
+//
+//  Created by Kevin Artan on 04/09/26.
+//
+
---
+++ Sources/AiDigestBot/Services/DigestArchiveStore.swift
@@ -0,0 +1,7 @@
+//
+//  DigestArchiveStore.swift
+//  AiDigestBot
+//
+//  Created by Kevin Artan on 04/09/26.
+//
+
